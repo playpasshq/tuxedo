047a16e98e35f3d28ff0b57f191e2bde6f8ba01d
Jan Stevens <jan@playpass.be>
2015-12-22 20:18:35 +0100

    Fixed typo in filename

--- lib/tuxedo/railtie.rb
+++ lib/tuxedo/railtie.rb
@@ -2,9 +2,8 @@ require 'rails/railtie'
 
 module Tuxedo
   class Railtie < ::Rails::Railtie
-    initializer 'tuxedo.action_view' do |_app|
+    initializer 'tuxedo.action_view' do
       ActiveSupport.on_load :action_view do
-        require 'tuxedo/action_view/helpers'
         include Tuxedo::ActionView::Helpers
       end
     end
